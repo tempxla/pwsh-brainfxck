04f73f49e1ac0e5cbffc0a26f57f7274951186ab
tempxla <38803749+tempxla@users.noreply.github.com>
2021-07-22 13:54:22 +0900

    Fix: Parameter Sysntax

--- Brainfxck.ps1
+++ Brainfxck.ps1
@@ -5,6 +5,9 @@ $ErrorActionPreference = 'Stop'
 
 [int]$script:BfPointer = 0
 
-function Initialize-BfRuntime ([int]$MemorySize = 5000) {
-    $script:BfMemory = @(1..$MemorySize) | ForEach-Object {0}
+function Initialize-BfRuntime {
+    param (
+        [int]$MemorySize = 5000
+    )
+    $script:BfMemory = @(1..$MemorySize) | ForEach-Object { 0 }
 }
